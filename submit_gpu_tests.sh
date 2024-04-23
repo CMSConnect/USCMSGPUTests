5e0a821293cc1f6cb63da0bb539ae9f0f4d87f0b
khurtado <khurtado@nd.edu>
2024-04-23 19:28:25 +0200

    Fix gatekeeper submission

--- submit_gpu_tests.sh
+++ submit_gpu_tests.sh
@@ -5,7 +5,7 @@
 # E.g.: cit-gatekeeper2.ultralight.org cit-gatekeeper2.ultralight.org:9619
 # The GWMS frontend won't match against the whole string (because the space splits both strings in two in their matching expression)
 # but it works if only one of the sub-strings is provided. Let's use the first, as recommended by the SI team
-condor_status -pool vocms0207.cern.ch -any -const 'MyType=?="glidefactory" && GLIDEIN_Supported_VOs=?="CMSGPU" && regexp("_US_",GLIDEIN_CMSSite)' -af GLIDEIN_Gatekeeper | awk '{print $2}' > site_gatekeepers.txt
+condor_status -pool vocms0207.cern.ch -any -const 'MyType=?="glidefactory" && GLIDEIN_Supported_VOs=?="CMSGPU" && regexp("_US_",GLIDEIN_CMSSite)' -af GLIDEIN_Gatekeeper | awk '{print $1}' > site_gatekeepers.txt
 #condor_status -pool vocms0207.cern.ch -any -const 'MyType=?="glidefactory" && GLIDEIN_Supported_VOs=?="CMSGPU" && regexp("_US_",GLIDEIN_CMSSite)' -af GLIDEIN_Gatekeeper > site_gatekeepers.txt
 
 # We can also submit at the site level
