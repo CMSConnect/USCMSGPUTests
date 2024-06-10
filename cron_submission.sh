24356dcf3279bb6a50aad36643a8190528a6878d
khurtado <khurtado@nd.edu>
2024-06-10 11:30:22 -0500

    Fix cron script

--- cron_submission.sh
+++ cron_submission.sh
@@ -4,7 +4,8 @@
 # $1=hostcert
 # $2=hostkey
 
-workDir=$PWD
+workDir=/connectuser/monitoring/USCMSGPUTests
+cd $workDir
 script=submit_gpu_tests.sh
 
 source  /cvmfs/oasis.opensciencegrid.org/mis/osg-wn-client/3.6/current/el7-x86_64/setup.sh
