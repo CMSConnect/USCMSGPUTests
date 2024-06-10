b4b3cfd893c73afda00e787f8e14bf0c93488d22
khurtado <khurtado@nd.edu>
2024-06-10 11:24:05 -0500

    Add script to be executed in a crontab.

---
+++ cron_submission.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# Specify keyless user cert to use
+# $1=hostcert
+# $2=hostkey
+
+workDir=$PWD
+script=submit_gpu_tests.sh
+
+source  /cvmfs/oasis.opensciencegrid.org/mis/osg-wn-client/3.6/current/el7-x86_64/setup.sh
+voms-proxy-init -voms cms -cert "$1" -key "$2" -out $workDir/tmp.proxy
+proxy=$workDir/tmp.proxy
+
+X509_USER_PROXY=$proxy sh $script
+rm $proxy
+
